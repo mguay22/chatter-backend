ee0d431cfbd99f492a532f098f6621f760fa6ef9
Michael <guay.me@gmail.com>
2023-09-04 10:53:47 -0400

    Apply local strategy

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { User } from '../users/entities/user.entity';
+
+@Controller('auth')
+export class AuthController {
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  async login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) response: Response,
+  ) {}
+}
---
+++ src/auth/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { User } from '../users/entities/user.entity';
+
+const getCurrentUserByContext = (context: ExecutionContext): User => {
+  return context.switchToHttp().getRequest().user;
+};
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) =>
+    getCurrentUserByContext(context),
+);
