e58c4d9498952523aa8659c0f5782e7f18f950f6
Michael <guay.me@gmail.com>
2023-09-20 17:00:01 -0400

    Improve auth error message

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -15,7 +15,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     try {
       return await this.usersService.verifyUser(email, password);
     } catch (err) {
-      throw new UnauthorizedException(err);
+      throw new UnauthorizedException('Credentials are not valid.');
     }
   }
 }
