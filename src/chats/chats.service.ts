1373a174caa047709633fff76fab447071c09b07
Michael <guay.me@gmail.com>
2023-10-28 17:09:59 -0400

    Get chats

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -15,8 +15,8 @@ export class ChatsService {
     });
   }
 
-  findAll() {
-    return `This action returns all chats`;
+  async findAll() {
+    return this.chatsRepository.find({});
   }
 
   findOne(id: number) {
