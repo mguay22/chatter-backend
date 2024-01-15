8717ab61cfeebba47af89aef7cdf9138c5a7610e
Michael <guay.me@gmail.com>
2024-01-15 18:55:25 -0500

    Redis pubsub

--- src/common/pubsub/pubsub.module.ts
+++ src/common/pubsub/pubsub.module.ts
@@ -1,13 +1,31 @@
 import { Global, Module } from '@nestjs/common';
 import { PUB_SUB } from '../constants/injection-tokens';
 import { PubSub } from 'graphql-subscriptions';
+import { ConfigService } from '@nestjs/config';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import Redis from 'ioredis';
+import { reviver } from './reviver';
 
 @Global()
 @Module({
   providers: [
     {
       provide: PUB_SUB,
-      useValue: new PubSub(),
+      useFactory: (configService: ConfigService) => {
+        if (configService.get('NODE_ENV') === 'production') {
+          const options = {
+            host: configService.getOrThrow('REDIS_HOST'),
+            port: configService.getOrThrow('REDIS_PORT'),
+          };
+          return new RedisPubSub({
+            publisher: new Redis(options),
+            subscriber: new Redis(options),
+            reviver: reviver,
+          });
+        }
+        return new PubSub();
+      },
+      inject: [ConfigService],
     },
   ],
   exports: [PUB_SUB],
---
+++ src/common/pubsub/reviver.ts
@@ -0,0 +1,16 @@
+import { Types } from 'mongoose';
+
+export const reviver = (key, value) => {
+  const isISO8601Z =
+    /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/;
+  if (typeof value === 'string' && isISO8601Z.test(value)) {
+    const tempDateNumber = Date.parse(value);
+    if (!isNaN(tempDateNumber)) {
+      return new Date(tempDateNumber);
+    }
+  }
+  if (key === '_id') {
+    return new Types.ObjectId(value);
+  }
+  return value;
+};
