6b391fbf6aad78e9e00061e0c12a14d3b8a864e9
Michael <guay.me@gmail.com>
2024-01-15 16:40:54 -0500

    Revert

--- src/common/pubsub/pubsub.module.ts
+++ src/common/pubsub/pubsub.module.ts
@@ -1,46 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { PUB_SUB } from '../constants/injection-tokens';
 import { PubSub } from 'graphql-subscriptions';
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import { ConfigService } from '@nestjs/config';
-import Redis from 'ioredis';
-import { Types } from 'mongoose';
-
-const reviver = (key, value) => {
-  const isISO8601Z =
-    /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/;
-  if (typeof value === 'string' && isISO8601Z.test(value)) {
-    const tempDateNumber = Date.parse(value);
-    if (!isNaN(tempDateNumber)) {
-      return new Date(tempDateNumber);
-    }
-  }
-  if (key === '_id') {
-    return new Types.ObjectId(value);
-  }
-  return value;
-};
 
 @Global()
 @Module({
   providers: [
     {
       provide: PUB_SUB,
-      useFactory: (configService: ConfigService) => {
-        if (configService.get('NODE_ENV') === 'production') {
-          const options = {
-            host: 'chatter-redis.ghk4hw.ng.0001.use1.cache.amazonaws.com',
-            port: 6379,
-          };
-          return new RedisPubSub({
-            publisher: new Redis(options),
-            subscriber: new Redis(options),
-            reviver,
-          });
-        }
-        return new PubSub();
-      },
-      inject: [ConfigService],
+      useValue: new PubSub(),
     },
   ],
   exports: [PUB_SUB],
