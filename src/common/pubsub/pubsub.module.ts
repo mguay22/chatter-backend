c5f46cf151c18fa6a21cff46fd9e57a4a5283bd0
Michael <guay.me@gmail.com>
2024-01-14 19:22:33 -0500

    Try redis pubsub

--- src/common/pubsub/pubsub.module.ts
+++ src/common/pubsub/pubsub.module.ts
@@ -1,13 +1,25 @@
 import { Global, Module } from '@nestjs/common';
 import { PUB_SUB } from '../constants/injection-tokens';
 import { PubSub } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
   providers: [
     {
       provide: PUB_SUB,
-      useValue: new PubSub(),
+      useFactory: (configService: ConfigService) => {
+        if (configService.get('NODE_ENV') === 'production') {
+          return new RedisPubSub({
+            connection: {
+              host: 'chatter-redis.ghk4hw.ng.0001.use1.cache.amazonaws.com',
+              port: 6379,
+            },
+          });
+        }
+        return new PubSub();
+      },
     },
   ],
   exports: [PUB_SUB],
