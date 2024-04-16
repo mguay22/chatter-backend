2e9f0df8cf96fa5f43c5a77b20b51029be4cbbb3
Michael <guay.me@gmail.com>
2024-04-16 18:09:58 -0400

    Bump packages to latest

--- src/common/database/abstract.repository.ts
+++ src/common/database/abstract.repository.ts
@@ -16,7 +16,7 @@ export abstract class AbstractRepository<T extends AbstractEntity> {
   }
 
   async findOne(filterQuery: FilterQuery<T>): Promise<T> {
-    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+    const document = await this.model.findOne(filterQuery, {}).lean<T>();
 
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
@@ -30,10 +30,11 @@ export abstract class AbstractRepository<T extends AbstractEntity> {
     filterQuery: FilterQuery<T>,
     update: UpdateQuery<T>,
   ): Promise<T> {
-    const document = await this.model.findOneAndUpdate(filterQuery, update, {
-      lean: true,
-      new: true,
-    });
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true,
+      })
+      .lean<T>();
 
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
@@ -44,10 +45,10 @@ export abstract class AbstractRepository<T extends AbstractEntity> {
   }
 
   async find(filterQuery: FilterQuery<T>): Promise<T[]> {
-    return this.model.find(filterQuery, {}, { lean: true });
+    return this.model.find(filterQuery).lean<T[]>();
   }
 
   async findOneAndDelete(filterQuery: FilterQuery<T>): Promise<T> {
-    return this.model.findOneAndDelete(filterQuery, { lean: true });
+    return this.model.findOneAndDelete(filterQuery).lean<T>();
   }
 }
